a6d12107c7d8389568706f7a6280b023d72ca362
KoichiOzaki <goingmyway19851224@gmail.com>
2017-01-15 16:43:48 +0900

    articlesテーブル作成

--- db/migrate/20170115073137_create_articles.rb
+++ db/migrate/20170115073137_create_articles.rb
@@ -1,6 +1,10 @@
 class CreateArticles < ActiveRecord::Migration[5.0]
   def change
     create_table :articles do |t|
+      t.string :title
+      #LONGTEXT-mysql
+      t.text :text, :limit => 4294967295
+      t.integer :status
 
       t.timestamps
     end
